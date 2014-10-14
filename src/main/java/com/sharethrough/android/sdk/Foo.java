277f1b11978c68a97242206cd2d5c97a55e978f5
Lenny and Ryan <pair+lenny+rloomba@sharethrough.com>
2014-10-14 09:46:12 -0700

    play file

---
+++ src/main/java/com/sharethrough/android/sdk/Foo.java
@@ -0,0 +1,9 @@
+package com.sharethrough.android.sdk;
+
+/**
+ * Created by pivotal on 10/13/14.
+ */
+public class Foo {
+    public static final String bar = "BAR";
+    public static final String sna = "SNA";
+}
