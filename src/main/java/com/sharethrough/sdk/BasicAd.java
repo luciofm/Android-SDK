cace963a33fd2b635a10321e27d8c8ee4bba82bd
Lenny and Ryan <pair+lenny+rloomba@sharethrough.com>
2014-10-16 14:37:34 -0700

    remove unnecessary include of test libs in the output archive

--- src/main/java/com/sharethrough/sdk/BasicAd.java
+++ src/main/java/com/sharethrough/sdk/BasicAd.java
@@ -2,8 +2,10 @@ package com.sharethrough.sdk;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class BasicAd extends FrameLayout {
     public BasicAd(Context context) {
@@ -31,4 +33,15 @@ public class BasicAd extends FrameLayout {
 //        adView.getTitle().setText(ad.getTitle());
     }
 
+    public void showAd(int layoutResourceId, int titleViewId, int descriptionViewId, int advertiserViewId) {
+        Sharethrough sharethrough = new Sharethrough("abc");// TODO
+
+        View view = LayoutInflater.from(getContext()).inflate(layoutResourceId, this);
+        Response.Creative creative = sharethrough.getCreative();
+        if (null != creative) {
+            ((TextView)view.findViewById(titleViewId)).setText(creative.creative.title);
+            ((TextView)view.findViewById(descriptionViewId)).setText(creative.creative.description);
+            ((TextView)view.findViewById(advertiserViewId)).setText(creative.creative.advertiser);
+        }
+    }
 }
