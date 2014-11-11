da24db5be2b1b48bb6c84aed5c8f2c7c0260c76b
Aaron VonderHaar and Lenny Turetsky <pair+avonderhaar+lenny@pivotallabs.com>
2014-11-11 11:52:45 -0800

    fire 50%, 75% and 95% completion beacon on Youtube videos

--- src/main/java/com/sharethrough/sdk/webview/YoutubeJavascriptInterface.java
+++ src/main/java/com/sharethrough/sdk/webview/YoutubeJavascriptInterface.java
@@ -11,6 +11,9 @@ public class YoutubeJavascriptInterface {
     private final Creative creative;
     private final BeaconService beaconService;
     private boolean quarterFired;
+    private boolean halfFired;
+    private boolean threeQuartersFired;
+    private boolean finishedFired;
 
     public YoutubeJavascriptInterface(Context context, Creative creative, BeaconService beaconService) {
         this.context = context;
@@ -21,9 +24,22 @@ public class YoutubeJavascriptInterface {
     @JavascriptInterface
     public void timeUpdate(double time, double duration) {
         Log.v("Sharethrough", creative.getCreativeKey() + " video has played " + time + "s of " + duration + "s");
-        if (!quarterFired && time * 4 > duration) {
+        double percent = time/duration;
+        if (!quarterFired && percent >= 0.25) {
             quarterFired = true;
             beaconService.videoPlayed(context, creative, 25);
+        }
+        if (!halfFired && percent >= 0.5) {
+            halfFired = true;
+            beaconService.videoPlayed(context, creative, 50);
+        }
+        if (!threeQuartersFired && percent >= 0.75) {
+            threeQuartersFired = true;
+            beaconService.videoPlayed(context, creative, 75);
+        }
+        if (!finishedFired && percent >= 0.95) {
+            finishedFired = true;
+            beaconService.videoPlayed(context, creative, 95);
         }
     }
 }
--- src/test/java/com/sharethrough/sdk/webview/YoutubeJavascriptInterfaceTest.java
+++ src/test/java/com/sharethrough/sdk/webview/YoutubeJavascriptInterfaceTest.java
@@ -10,9 +10,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
@@ -29,13 +27,59 @@ public class YoutubeJavascriptInterfaceTest extends TestCase {
         subject = new YoutubeJavascriptInterface(Robolectric.application, creative, beaconService);
     }
 
-    @Test
-    public void firesBeaconWhenFirstQuarterHasPlayed_onlyOnce() throws Exception {
+    public void timeUpdate_before25PercentPlayed_firesNoBeacons() throws Exception {
         subject.timeUpdate(0.9, 4.0);
         verifyNoMoreInteractions(beaconService);
+    }
+
+    @Test
+    public void firesBeaconWhenFirstQuarterHasPlayed_onlyOnce() throws Exception {
         subject.timeUpdate(1.1, 4.0);
         verify(beaconService).videoPlayed(Robolectric.application, creative, 25);
-        subject.timeUpdate(1.1, 4.0);
+        verifyNoMoreInteractions(beaconService);
+    }
+
+    @Test
+    public void timeUpdate_whenHalfPlayed_firesCorrectBeacons() throws Exception {
+        subject.timeUpdate(1.9, 4.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 25);
+        verifyNoMoreInteractions(beaconService);
+        subject.timeUpdate(2.0, 4.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 50);
+        verifyNoMoreInteractions(beaconService);
+    }
+
+    @Test
+    public void timeUpdate_when75PercentPlayed_firesCorrectBeacons() throws Exception {
+        subject.timeUpdate(2.99, 4.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 25);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 50);
+        verifyNoMoreInteractions(beaconService);
+        subject.timeUpdate(3.00, 4.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 75);
+        verifyNoMoreInteractions(beaconService);
+    }
+
+    @Test
+    public void timeUpdate_when95PercentPlayed_firesCorrectBeacons() throws Exception {
+        subject.timeUpdate(9.49, 10.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 25);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 50);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 75);
+        verifyNoMoreInteractions(beaconService);
+        subject.timeUpdate(9.5, 10.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 95);
+        verifyNoMoreInteractions(beaconService);
+    }
+
+    @Test
+    public void timeUpdate_onlyFiresBeaconsOnce() throws Exception {
+        subject.timeUpdate(3.99, 4.0);
+        subject.timeUpdate(3.99, 4.0);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 25);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 50);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 75);
+        verify(beaconService).videoPlayed(Robolectric.application, creative, 95);
         verifyNoMoreInteractions(beaconService);
     }
 }
