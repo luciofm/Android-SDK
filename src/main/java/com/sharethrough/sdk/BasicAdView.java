736822adfeba714c8550ddb2303cd605341a9757
Lenny Turetsky and Shane Blackie <engineers+sweetleon+sblackie@sharethrough.com>
2014-11-17 11:39:25 -0800

    show Optout button linking to privacy policy

--- src/main/java/com/sharethrough/sdk/BasicAdView.java
+++ src/main/java/com/sharethrough/sdk/BasicAdView.java
@@ -1,14 +1,18 @@
 package com.sharethrough.sdk;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import com.sharethrough.android.sdk.R;
 
 public class BasicAdView extends FrameLayout implements IAdView {
     private int titleViewId;
@@ -16,6 +20,7 @@ public class BasicAdView extends FrameLayout implements IAdView {
     private int advertiserViewId;
     private int thumbnailViewId;
     private View view;
+    private FrameLayout adHolder;
 
     public BasicAdView(Context context) {
         super(context);
@@ -34,16 +39,33 @@ public class BasicAdView extends FrameLayout implements IAdView {
     }
 
     public BasicAdView showAd(Sharethrough sharethrough, final int layoutResourceId, final int titleViewId, final int descriptionViewId, final int advertiserViewId, final int thumbnailViewId) {
+        adHolder = new FrameLayout(getContext());
+        addView(adHolder);
+
         this.titleViewId = titleViewId;
         this.descriptionViewId = descriptionViewId;
         this.advertiserViewId = advertiserViewId;
         this.thumbnailViewId = thumbnailViewId;
         view = LayoutInflater.from(getContext()).inflate(layoutResourceId, this, false);
-        addView(new ProgressBar(getContext()), new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        adHolder.addView(new ProgressBar(getContext()), new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        final ImageView optout = new ImageView(getContext());
+        optout.setImageResource(R.drawable.optout);
+        optout.setTag("SHARETHROUGH PRIVACY INFORMATION");
+        optout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent privacyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sharethrough.com/privacy-policy/"));
+                v.getContext().startActivity(privacyIntent);
+            }
+        });
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.BOTTOM | Gravity.RIGHT);
+        layoutParams.setMargins(0, 0, 10, 10);
+        addView(optout, layoutParams);
+
         sharethrough.putCreativeIntoAdView(this, new Runnable() {
             @Override
             public void run() {
-                addView(view);
+                adHolder.addView(view);
             }
         });
         return this;
--- src/test/java/com/sharethrough/sdk/BasicAdViewTest.java
+++ src/test/java/com/sharethrough/sdk/BasicAdViewTest.java
@@ -1,40 +1,61 @@
 package com.sharethrough.sdk;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 import android.widget.ProgressBar;
 import com.sharethrough.android.sdk.R;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.robolectric.Robolectric;
 
 import static com.sharethrough.test.util.Misc.findViewOfType;
 import static org.fest.assertions.api.ANDROID.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.robolectric.Robolectric.shadowOf;
 
 public class BasicAdViewTest extends TestBase {
+
+    private BasicAdView subject;
+    private Sharethrough sharethrough;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new BasicAdView(Robolectric.application);
+        sharethrough = mock(Sharethrough.class);
+        subject.showAd(sharethrough, R.layout.ad, R.id.title, R.id.description, R.id.advertiser, R.id.thumbnail);
+    }
+
     @Test
     public void showAd_tellsSharethroughToShowTheAd() throws Exception {
-        BasicAdView subject = new BasicAdView(Robolectric.application);
-        Sharethrough sharethrough = mock(Sharethrough.class);
-        subject.showAd(sharethrough, R.layout.dialog, 2, 3, 4, 5);
         verify(sharethrough).putCreativeIntoAdView(eq(subject), any(Runnable.class));
     }
 
     @Test
     public void showsSpinner_untilAdIsReady() throws Exception {
-        BasicAdView subject = new BasicAdView(Robolectric.application);
-        Sharethrough sharethrough = mock(Sharethrough.class);
-        subject.showAd(sharethrough, R.layout.ad, R.id.title, R.id.description, R.id.advertiser, R.id.thumbnail);
         ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
-        verify(sharethrough).putCreativeIntoAdView(eq(subject), runnableArgumentCaptor.capture());
+        verify(sharethrough).putCreativeIntoAdView(same(subject), runnableArgumentCaptor.capture());
 
-        assertThat(findViewOfType(ProgressBar.class, subject)).isNotNull();
+        ProgressBar spinner = findViewOfType(ProgressBar.class, subject);
+        assertThat(spinner).isNotNull();
+        assertThat(spinner.getParent()).isNotSameAs(subject);
+
         assertThat(subject.getTitle()).isNull();
-
         runnableArgumentCaptor.getValue().run();
-
         assertThat(subject.getTitle()).isNotNull();
+
+        assertThat(subject.getTitle().getParent().getParent()).isSameAs(spinner.getParent());
+    }
+
+    @Test
+    public void hasOptoutButton_thatLinksToPrivacyInformation() throws Exception {
+        View optout = subject.findViewWithTag("SHARETHROUGH PRIVACY INFORMATION");
+        assertThat(optout.getParent()).isSameAs(subject);
+        optout.performClick();
+        assertThat(shadowOf(Robolectric.application).getNextStartedActivity()).isEqualTo(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.sharethrough.com/privacy-policy/")));
     }
 }
