f7aeda403794c5a30224fdb70b24f0eac4be00d1
Ryan Loomba <engineers+rloomba@sharethrough.com>
2015-01-16 17:50:41 -0800

    include version in UA string

--- src/main/java/com/sharethrough/sdk/Sharethrough.java
+++ src/main/java/com/sharethrough/sdk/Sharethrough.java
@@ -24,7 +24,8 @@ import java.util.concurrent.TimeUnit;
 public class Sharethrough {
     public static final int DEFAULT_AD_CACHE_TIME_IN_MILLISECONDS = (int) TimeUnit.SECONDS.toMillis(20);
     private static final int MINIMUM_AD_CACHE_TIME_IN_MILLISECONDS = (int) TimeUnit.SECONDS.toMillis(20);
-    public static final String USER_AGENT = System.getProperty("http.agent");
+    private static final String SDK_VERSION_NUMBER = "1.0.2";
+    public static String USER_AGENT = System.getProperty("http.agent") + "; STR " + SDK_VERSION_NUMBER;
     private final Renderer renderer;
     private final BeaconService beaconService;
     private final int adCacheTimeInMilliseconds;
