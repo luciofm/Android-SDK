b0d6669c011c5aaa171c8d7135c87212db9c7036
Danica Shei and Ryan Loomba <engineers+danica+ryan@sharethrough.com>
2015-04-09 15:15:15 -0700

    trim white space and '/' from beginning and end of url, then prepend 'http" if url is relative

--- src/main/java/com/sharethrough/sdk/dialogs/ShareableDialog.java
+++ src/main/java/com/sharethrough/sdk/dialogs/ShareableDialog.java
@@ -99,9 +99,17 @@ public abstract class ShareableDialog extends Dialog {
             cancel();
             return true;
         } else if (item.getItemId() == R.id.menu_item_custom) {
-            final String url = getCreative().getCustomEngagementUrl();
             final Intent intent = new Intent();
-            intent.setData(Uri.parse(url));
+
+            final String url = getCreative().getCustomEngagementUrl();
+            String pattern = "(^(\\s|/)+)|((\\s|/)+$)";
+            String canonicalUrl = url.replaceAll(pattern, "");
+            if (Uri.parse(canonicalUrl).isAbsolute()) {
+                intent.setData(Uri.parse(url));
+            } else {
+                intent.setData(Uri.parse("http://" + canonicalUrl));
+            }
+
             getContext().startActivity(intent);
 
             return true;
