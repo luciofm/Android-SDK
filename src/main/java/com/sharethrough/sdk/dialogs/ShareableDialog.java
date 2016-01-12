b77e6bd5b23fd502f91157c15ea454224aefb79a
Danica Shei <engineers+danica@sharethrough.com>
2016-01-11 21:43:44 -0800

    Lock orientation when ad dialog created, unlock when dialog is dismissed

--- src/main/java/com/sharethrough/sdk/dialogs/ShareableDialog.java
+++ src/main/java/com/sharethrough/sdk/dialogs/ShareableDialog.java
@@ -2,19 +2,20 @@ package com.sharethrough.sdk.dialogs;
 
 import android.annotation.TargetApi;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.Window;
+import android.view.*;
 import android.widget.ShareActionProvider;
 import com.sharethrough.android.sdk.R;
 import com.sharethrough.sdk.BeaconService;
@@ -27,7 +28,7 @@ import java.util.List;
  this base class provides beacon firing when a user tries to share the ad content with another android package.
  */
 
-public abstract class ShareableDialog extends Dialog {
+public abstract class ShareableDialog extends Dialog implements DialogInterface.OnDismissListener {
     protected final BeaconService beaconService;
     protected final int feedPosition;
 
@@ -36,6 +37,28 @@ public abstract class ShareableDialog extends Dialog {
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
         this.beaconService = beaconService;
         this.feedPosition = feedPosition;
+
+        if( context instanceof Activity != true ) {
+            return;
+        }
+
+        //lock screen orientation
+        Activity activity = (Activity) context;
+        int currentOrientation = context.getResources().getConfiguration().orientation;
+
+        if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        } else {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        }
+        this.setOnDismissListener(this);
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        Context themeWrappedContext = ((ContextThemeWrapper)getContext()).getBaseContext();
+        Activity activity = (Activity) themeWrappedContext;
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
@@ -71,7 +94,7 @@ public abstract class ShareableDialog extends Dialog {
             public boolean onShareTargetSelected(ShareActionProvider source, Intent intent) {
                 String packageName = "" + intent.getComponent().getPackageName();
 
-                PackageManager pkgManager = getContext().getPackageManager();
+                PackageManager pkgManager = getContext().getApplicationContext().getPackageManager();
                 List<ResolveInfo> resolveInfos = pkgManager.queryIntentActivities(new Intent(Intent.ACTION_SEND).setType("message/rfc822"), 0);
                 for (ResolveInfo resolveInfo : resolveInfos) {
                     if (packageName.equals(resolveInfo.activityInfo.packageName)) {
