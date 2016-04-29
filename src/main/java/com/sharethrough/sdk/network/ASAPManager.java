05ab004505f486e0104da76e5ca5346f66d0bfbc
Danica Shei <engineers+danica@sharethrough.com>
2016-04-29 11:41:40 -0700

    ASAPManager responds accordingly to asap response

--- src/main/java/com/sharethrough/sdk/network/ASAPManager.java
+++ src/main/java/com/sharethrough/sdk/network/ASAPManager.java
@@ -5,21 +5,19 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 
 /**
  * Created by engineers on 4/26/16.
  */
 public class ASAPManager {
-
     private static final String ASAP_ENDPOINT = "https://asap-staging.sharethrough.com/v1";
-    private static final String DFP_CREATIVE_KEY = "creative_key";
-    private static final String DFP_CAMPAIGN_KEY = "campaign_key";
     private String placementKey;
     private RequestQueue requestQueue;
 
@@ -29,15 +27,25 @@ public class ASAPManager {
     }
 
     public void callASAP(final ASAPManagerListener asapManagerListener) {
-        // Instantiate the RequestQueue.
         String url = ASAP_ENDPOINT + "?pkey=" + placementKey;
 
-        // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        asapManagerListener.onSuccess(parseAsapResponse(response));
+                        Gson gson = new Gson();
+                        AdResponse adResponse = gson.fromJson(response, AdResponse.class);
+
+                        if (Arrays.asList(adResponse.errorRange).contains(adResponse.responseCode)) {
+                            asapManagerListener.onError(adResponse.responseError);
+                        } else {
+                            ArrayList<NameValuePair> queryStringParams = new ArrayList<NameValuePair>();
+                            queryStringParams.add(new BasicNameValuePair("placement_key", placementKey));
+                            if (adResponse.requestTypeName != null && adResponse.requestTypeValue != null) {
+                                queryStringParams.add(new BasicNameValuePair(adResponse.requestTypeName, adResponse.requestTypeValue));
+                            }
+                            asapManagerListener.onSuccess(queryStringParams);
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -46,7 +54,6 @@ public class ASAPManager {
             }
         });
 
-        // Add the request to the RequestQueue.
         requestQueue.add(stringRequest);
     }
 
@@ -55,32 +62,12 @@ public class ASAPManager {
         void onError(String error);
     }
 
-    public ArrayList<NameValuePair> parseAsapResponse(String asapResponseString) {
-        HashMap<String, String> asapKeys= new HashMap<String, String>();
-        if (asapResponseString.contains(DFP_CREATIVE_KEY)) {
-            String[] tokens = asapResponseString.split("=");
-            asapKeys.put(DFP_CREATIVE_KEY, tokens[1]);
-        } else if (asapResponseString.contains(DFP_CAMPAIGN_KEY)) {
-            String[] tokens = asapResponseString.split("=");
-            asapKeys.put(DFP_CAMPAIGN_KEY, tokens[1]);
-        } else {
-            asapKeys.put(DFP_CREATIVE_KEY, asapResponseString);
-        }
-
-        ArrayList<NameValuePair> queryStringParams = new ArrayList<NameValuePair>(2);
-        queryStringParams.add(new BasicNameValuePair("placement_key", placementKey));
-        if (asapKeys.containsKey(DFP_CREATIVE_KEY)) {
-            String creativeKey = asapKeys.get(DFP_CREATIVE_KEY);
-            if (!creativeKey.equals("STX_MONETIZE")){
-                queryStringParams.add(new BasicNameValuePair("creative_key", creativeKey));
-            }
-        } else if (asapKeys.containsKey(DFP_CAMPAIGN_KEY)) {
-            String campaignKey = asapKeys.get(DFP_CAMPAIGN_KEY);
-            if (!campaignKey.equals("STX_MONETIZE")) {
-                queryStringParams.add(new BasicNameValuePair("campaign_key", campaignKey));
-            }
-        }
-
-        return queryStringParams;
+    public class AdResponse {
+        String placementKey;
+        String requestTypeName;
+        String requestTypeValue;
+        int responseCode;
+        String responseError;
+        int[] errorRange;
     }
 }
