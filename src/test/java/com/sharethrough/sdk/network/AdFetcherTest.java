a6c24f3c357cc2303a5bffbb3c1d72c75b6fbf39
Danica Shei <engineers+danica@sharethrough.com>
2016-05-02 12:52:25 -0700

    Cleaned up AdFetcherTest

--- src/test/java/com/sharethrough/sdk/network/AdFetcherTest.java
+++ src/test/java/com/sharethrough/sdk/network/AdFetcherTest.java
@@ -1,29 +1,14 @@
 package com.sharethrough.sdk.network;
 
 import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.sharethrough.sdk.*;
 import com.sharethrough.test.Fixtures;
-import com.sharethrough.test.util.Misc;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.robolectric.Robolectric;
-import org.robolectric.shadows.ShadowLog;
-import org.robolectric.tester.org.apache.http.TestHttpResponse;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -32,97 +17,56 @@ public class AdFetcherTest extends TestBase {
     private static final String SINGLE_LAYOUT_FIXTURE = Fixtures.getFile("assets/str_single_ad_youtube.json");
     private static final String NO_CREATIVE_FIXTURE = Fixtures.getFile("assets/str_no_creatives.json");
     private static final String PRE_LIVE_PLACEMENT_FIXTURE = Fixtures.getFile("assets/str_prelive_placement.json");
-    @Mock private AdFetcher.AdFetcherListener adfetcherlistener;
+
     private AdFetcherStub subject;
-    private String apiUri;
-    private String key;
+    @Mock private AdFetcher.AdFetcherListener adfetcherlistener;
+    @Mock private RequestQueue requestQueue;
 
-    private String expectedUri;
-    private ArrayList<NameValuePair> queryStringParams;
-    private ArrayList<NameValuePair> expectedStringParams;
-
-    class AdFetcherStub extends  AdFetcher{
+    class AdFetcherStub extends AdFetcher {
         public AdFetcherStub(Context context) {
             super(context);
         }
 
-        public void fetchAds(String adRequestUrl){
-            /*SendHttpRequestTask sendHttpRequestTask = new SendHttpRequestTask();
-            try {
-                sendHttpRequestTask.execute(adRequestUrl).get();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }*/
+        public void setRequestQueue(RequestQueue requestQueue) {
+            this.requestQueue = requestQueue;
         }
     }
-
 
     @Before
     public void setUp() throws Exception {
         Logger.enabled = true;
         subject = new AdFetcherStub(Robolectric.application);
-        apiUri = "http://api";
-        key = "key";
-        queryStringParams = new ArrayList<NameValuePair>(1);
-        queryStringParams.add(new BasicNameValuePair("key", key));
-
-        expectedStringParams = (ArrayList<NameValuePair>) queryStringParams.clone();
-        expectedStringParams.add(new BasicNameValuePair("uid", "fake-uid"));
-        expectedStringParams.add(new BasicNameValuePair("appId", "version-number"));
-        expectedStringParams.add(new BasicNameValuePair("appName", "com.sharethrough.example"));
-        expectedUri = apiUri + "?" + URLEncodedUtils.format(expectedStringParams, "utf-8");
-
+        subject.setRequestQueue(requestQueue);
         subject.setAdFetcherListener(adfetcherlistener);
     }
 
     @Test
-    public void fetchAds_whenServerReturnsAds_callBackWithJsonResponse() throws  Exception{
-        Robolectric.addHttpResponseRule("GET", expectedUri, new TestHttpResponse(200, SINGLE_LAYOUT_FIXTURE));
-        subject.fetchAds(expectedUri);
+    public void fetchAds_triggersAdRequest() throws Exception {
+        subject.fetchAds("url");
+
+        verify(requestQueue).add((Request) anyObject());
+    }
+
+    @Test
+    public void handleResponse_callBackWithAdResponseLoaded_whenJsonIsValid() throws Exception {
+        subject.handleResponse(SINGLE_LAYOUT_FIXTURE);
+
         verify(adfetcherlistener).onAdResponseLoaded((Response) anyObject());
     }
 
-
     @Test
-    public void fetchAds_whenServerRequestFails_callBackWithAdsFailedToLoad() throws Exception {
-        Robolectric.addHttpResponseRule("GET", expectedUri, new TestHttpResponse(500, "Bad server"));
-        subject.fetchAds(expectedUri);
-        verify(adfetcherlistener).onAdResponseFailed();
+    public void handleResponse_callBackWithAdResponseLoaded_whenJsonIsValidButNoAds() throws Exception {
+        subject.handleResponse(NO_CREATIVE_FIXTURE);
+
+        verify(adfetcherlistener).onAdResponseLoaded((Response) anyObject());
     }
 
     @Test
-    public void fetchAds_whenExceptionOccursBeforeGettingHttpResponse_callBackWithAdsFailedToLoad() throws Exception {
-        Robolectric.addHttpResponseRule("GET", expectedUri, null);
-
-        subject.fetchAds(expectedUri);
-        verify(adfetcherlistener).onAdResponseFailed();
-    }
-
-    @Test
-    public void fetchAds_whenExceptionOccursAfterGettingHttpResponse_callBackWithAdsFailedToLoad() throws Exception {
+    public void handleResponse_callBackWithAdResponseFailed_whenJsonIsInvalid() throws Exception {
         String responseBody = "{234789wdfjkl ";
-        Robolectric.addHttpResponseRule("GET", expectedUri, new TestHttpResponse(200, responseBody));
+        subject.handleResponse(responseBody);
 
-        subject.fetchAds(expectedUri);
         verify(adfetcherlistener).onAdResponseFailed();
-    }
-
-
-    @Test
-    public void whenServerReturns204_callBackWithJsonResponse() throws Exception {
-        Robolectric.addHttpResponseRule("GET", expectedUri, new TestHttpResponse(204, "I got nothing for ya"));
-
-        subject.fetchAds(expectedUri);
-        verify(adfetcherlistener).onAdResponseFailed();
-    }
-
-    @Test
-    public void fetchAds_whenServerReturnsNoAds_callBackWithJsonResponse() throws  Exception{
-        Robolectric.addHttpResponseRule("GET", expectedUri, new TestHttpResponse(200, NO_CREATIVE_FIXTURE));
-        subject.fetchAds(expectedUri);
-        verify(adfetcherlistener).onAdResponseLoaded((Response) anyObject());
     }
 
     @Test
