a443a177f1e9438e4e39b5b4f3cf505c6c628ef3
Jermaine Chan <engineers+jermaine@sharethrough.com>
2016-08-08 11:25:56 -0700

    fix test for ad cache timer

--- src/test/java/com/sharethrough/sdk/AdViewTimerTaskTest.java
+++ src/test/java/com/sharethrough/sdk/AdViewTimerTaskTest.java
@@ -45,7 +45,6 @@ public class AdViewTimerTaskTest extends TestBase {
             }
         };
         sharethrough = mock(Sharethrough.class);
-        when(sharethrough.getAdCacheTimeInMilliseconds()).thenReturn((long) TimeUnit.SECONDS.toMillis(20));
         sharethrough.placement = placement;
 
         subject = new AdViewTimerTask(adView, feedPosition, creative, beaconService, dateProvider, sharethrough);
