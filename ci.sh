9a49fd21c635db9fc771c7ea1267302e64156204
Peter Kinmond <peterkinmond@gmail.com>
2016-09-08 17:33:38 -0700

    Make logic simpler for Travis states

--- ci.sh
+++ ci.sh
@@ -1,16 +1,13 @@
 response=`curl https://api.travis-ci.org/repos/sharethrough/Android-SDK/branches/master`
 
 state=`echo $response | jq '.branch.state'`
+echo $state
 
-if [ "$state" = '"passed"' ]
+if [ "$state" = '"failed"' ]
 	then
-	echo build has passed
-	exit 0
-elif [ "$state" = '"created"' ]
-	then
-	echo build is pending
-    exit 0
-else 
-	echo failed
+	echo build has failed
 	exit 1
+else 
+	echo build has passed or is pending
+	exit 0
 fi
